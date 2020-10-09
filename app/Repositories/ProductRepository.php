1d6aaf0ca86e6a1ecd3b3f23caf2e1bf037bbf99
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-10-09 23:57:13 +0700

    create production repository with create function

---
+++ app/Repositories/ProductRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Product;
+use Illuminate\Support\Str;
+
+class ProductRepository
+{
+    public static function store($data, $name)
+    {
+        return Product::create([
+            'product_name' => $data['product_name'],
+            'price' => $data['price'],
+            'stock' => $data['stock'],
+            'description' => $data['description'],
+            'slug' => Str::slug($data['product_name']),
+            'picture' => $name
+        ]);
+    }
+}
