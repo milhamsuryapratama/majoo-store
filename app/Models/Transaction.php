da76379635039921bcece2fcca88eab9fec10218
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-10-10 06:17:37 +0700

    create transaction and transaction detail model

---
+++ app/Models/Transaction.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'total',
+        'province',
+        'city',
+        'postcode',
+        'address',
+        'status',
+        'payment_process',
+        'file'
+    ];
+
+    public function details()
+    {
+        return $this->hasMany(Transaction_Details::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    function getAddress()
+    {
+        return $this->attributes['address'].', '.$this->attributes['city'].', '.$this->attributes['province'].', '.$this->attributes['postcode'];
+    }
+}
