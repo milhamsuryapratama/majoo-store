3a104b0a352caf0ed6739f71d6eb82f455e77123
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-10-10 09:00:16 +0700

    fixing logo and active menu sidebar

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -36,35 +36,29 @@
 <body class="animsition">
 <div class="page-wrapper">
     <aside class="menu-sidebar2">
-        <div class="logo">
+        <div class="logo" style="background-color: white">
             <a href="#">
-                <img src="{{ asset('assets/admin/images/icon/logo-white.png') }}" alt="Majoo Store"/>
+                <img src="https://majoo.id/assets/dist/img/majoo@2x.png" alt="Majoo Store" width="150"/>
             </a>
         </div>
         <div class="menu-sidebar2__content js-scrollbar1">
             <div class="account2">
-                <div class="image img-cir img-120">
-                    <img src="{{ asset('assets/admin/images/icon/avatar-big-01.jpg') }}" alt="{{ Auth::guard('admin')->user()->name }}"/>
+                <div class="image img-120">
+                    <img src="https://majoo.id/assets/dist/img/majoo@2x.png"
+                         alt="{{ Auth::guard('admin')->user()->name }}"/>
                 </div>
                 <h4 class="name">{{ Auth::guard('admin')->user()->name }}</h4>
                 <a href="{{ URL::to('admin/logout') }}">Sign out</a>
             </div>
             <nav class="navbar-sidebar2">
                 <ul class="list-unstyled navbar__list">
-                    <li class="active has-sub">
-                        <a class="js-arrow" href="#">
-                            <i class="fas fa-tachometer-alt"></i>Dashboard
-                        </a>
-                    </li>
-                    <li>
+                    <li class="{{ Request::segment(2) == 'products' ? 'active': '' }}">
                         <a href="{{ route('products.index') }}">
                             <i class="fas fa-chart-bar"></i>Products</a>
-                        <span class="inbox-num">3</span>
                     </li>
-                    <li>
+                    <li class="{{ Request::segment(2) == 'transaction' ? 'active': '' }}">
                         <a href="{{ URL::to('admin/transaction') }}">
                             <i class="fas fa-chart-bar"></i>Transactions</a>
-                        <span class="inbox-num">3</span>
                     </li>
                     <li>
                         <a href="#">
@@ -83,97 +77,8 @@
                     <div class="header-wrap2">
                         <div class="logo d-block d-lg-none">
                             <a href="#">
-                                <img src="images/icon/logo-white.png" alt="CoolAdmin"/>
+                                <img src="https://majoo.id/assets/dist/img/majoo@2x.png" alt="CoolAdmin"/>
                             </a>
-                        </div>
-                        <div class="header-button2">
-                            <div class="header-button-item js-item-menu">
-                                <i class="zmdi zmdi-search"></i>
-                                <div class="search-dropdown js-dropdown">
-                                    <form action="">
-                                        <input class="au-input au-input--full au-input--h65" type="text"
-                                               placeholder="Search for datas &amp; reports..."/>
-                                        <span class="search-dropdown__icon">
-                                                <i class="zmdi zmdi-search"></i>
-                                            </span>
-                                    </form>
-                                </div>
-                            </div>
-                            <div class="header-button-item has-noti js-item-menu">
-                                <i class="zmdi zmdi-notifications"></i>
-                                <div class="notifi-dropdown js-dropdown">
-                                    <div class="notifi__title">
-                                        <p>You have 3 Notifications</p>
-                                    </div>
-                                    <div class="notifi__item">
-                                        <div class="bg-c1 img-cir img-40">
-                                            <i class="zmdi zmdi-email-open"></i>
-                                        </div>
-                                        <div class="content">
-                                            <p>You got a email notification</p>
-                                            <span class="date">April 12, 2018 06:50</span>
-                                        </div>
-                                    </div>
-                                    <div class="notifi__item">
-                                        <div class="bg-c2 img-cir img-40">
-                                            <i class="zmdi zmdi-account-box"></i>
-                                        </div>
-                                        <div class="content">
-                                            <p>Your account has been blocked</p>
-                                            <span class="date">April 12, 2018 06:50</span>
-                                        </div>
-                                    </div>
-                                    <div class="notifi__item">
-                                        <div class="bg-c3 img-cir img-40">
-                                            <i class="zmdi zmdi-file-text"></i>
-                                        </div>
-                                        <div class="content">
-                                            <p>You got a new file</p>
-                                            <span class="date">April 12, 2018 06:50</span>
-                                        </div>
-                                    </div>
-                                    <div class="notifi__footer">
-                                        <a href="#">All notifications</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="header-button-item mr-0 js-sidebar-btn">
-                                <i class="zmdi zmdi-menu"></i>
-                            </div>
-                            <div class="setting-menu js-right-sidebar d-none d-lg-block">
-                                <div class="account-dropdown__body">
-                                    <div class="account-dropdown__item">
-                                        <a href="#">
-                                            <i class="zmdi zmdi-account"></i>Account</a>
-                                    </div>
-                                    <div class="account-dropdown__item">
-                                        <a href="#">
-                                            <i class="zmdi zmdi-settings"></i>Setting</a>
-                                    </div>
-                                    <div class="account-dropdown__item">
-                                        <a href="#">
-                                            <i class="zmdi zmdi-money-box"></i>Billing</a>
-                                    </div>
-                                </div>
-                                <div class="account-dropdown__body">
-                                    <div class="account-dropdown__item">
-                                        <a href="#">
-                                            <i class="zmdi zmdi-globe"></i>Language</a>
-                                    </div>
-                                    <div class="account-dropdown__item">
-                                        <a href="#">
-                                            <i class="zmdi zmdi-pin"></i>Location</a>
-                                    </div>
-                                    <div class="account-dropdown__item">
-                                        <a href="#">
-                                            <i class="zmdi zmdi-email"></i>Email</a>
-                                    </div>
-                                    <div class="account-dropdown__item">
-                                        <a href="#">
-                                            <i class="zmdi zmdi-notifications"></i>Notifications</a>
-                                    </div>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
@@ -182,7 +87,7 @@
         <aside class="menu-sidebar2 js-right-sidebar d-block d-lg-none">
             <div class="logo">
                 <a href="#">
-                    <img src="images/icon/logo-white.png" alt="Cool Admin"/>
+                    <img src="https://majoo.id/assets/dist/img/majoo@2x.png" alt="Cool Admin"/>
                 </a>
             </div>
             <div class="menu-sidebar2__content js-scrollbar2">
