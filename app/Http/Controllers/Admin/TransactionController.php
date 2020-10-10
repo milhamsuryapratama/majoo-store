912b7452e5709de1b0d2ccf93c52ea5991e92783
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-10-10 08:20:48 +0700

    create transaction controller

---
+++ app/Http/Controllers/Admin/TransactionController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Transaction;
+use App\Services\TransactionService;
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+    public function index()
+    {
+        $data['transactions'] = Transaction::orderBy('id', 'DESC')->get();
+        return view('admin.transaction.index', $data);
+    }
+
+    public function detail($id)
+    {
+        $data['detail'] = Transaction::with(['details', 'details.product'])->findOrFail($id);
+        return view('admin.transaction.detail', $data);
+    }
+
+    public function delivered($id)
+    {
+        try {
+            TransactionService::validatePayment($id);
+            return redirect()->back()->with('success', 'Order has been delivered');
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'Cannot delivered, this order has not been paid yet');
+        }
+    }
+}
