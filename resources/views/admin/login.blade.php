913a3f830d07617a597c2335eca1ddc84d9f10be
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-10-10 09:22:37 +0700

    fixing admin login page

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -41,7 +41,7 @@
                 <div class="login-content">
                     <div class="login-logo">
                         <a href="#">
-                            <img src="images/icon/logo.png" alt="CoolAdmin">
+                            <img src="https://majoo.id/assets/dist/img/majoo@2x.png" alt="Majoo Store">
                         </a>
                     </div>
                     <div class="login-form">
@@ -55,22 +55,8 @@
                                 <label>Password</label>
                                 <input class="au-input au-input--full" type="password" name="password" placeholder="Password">
                             </div>
-                            <div class="login-checkbox">
-                                <label>
-                                    <input type="checkbox" name="remember">Remember Me
-                                </label>
-                                <label>
-                                    <a href="#">Forgotten Password?</a>
-                                </label>
-                            </div>
                             <button class="au-btn au-btn--block au-btn--green m-b-20" type="submit">sign in</button>
                         </form>
-                        <div class="register-link">
-                            <p>
-                                Don't you have account?
-                                <a href="#">Sign Up Here</a>
-                            </p>
-                        </div>
                     </div>
                 </div>
             </div>
