18b336263c83bd027d6196fe6048ac73ad32cab7
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-10-12 11:36:52 +0700

    comment email help text

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
                         <div class="form-group">
                             <label for="exampleInputEmail1">Email address</label>
                             <input type="email" class="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp" placeholder="Enter email">
-                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+{{--                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>--}}
                         </div>
                         <div class="form-group">
                             <label for="exampleInputPassword1">Password</label>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,25 +5,25 @@
         <div class="row justify-content-center">
             <div class="card">
                 <div class="card-header">
-                    Login
+                    Register
                 </div>
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
                         <div class="form-group">
                             <label for="name">Name</label>
-                            <input type="text" class="form-control" id="name" placeholder="Enter your name" name="name">
-                            <small id="nameHelp" class="form-text text-muted">{{ $errors ? $errors->first('name') : '' }}</small>
+                            <input type="text" class="form-control" id="name" placeholder="Enter your name" name="name" value="{{ old('name') }}">
+                            <small id="nameHelp" class="form-text" style="color: red">{{ $errors ? $errors->first('name') : '' }}</small>
                         </div>
                         <div class="form-group">
                             <label for="exampleInputEmail1">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" placeholder="Enter email">
-                            <small id="emailHelp" class="form-text text-muted">{{ $errors ? $errors->first('email') : '' }}</small>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" placeholder="Enter email" value="{{ old('email') }}">
+                            <small id="emailHelp" class="form-text" style="color: red">{{ $errors ? $errors->first('email') : '' }}</small>
                         </div>
                         <div class="form-group">
                             <label for="exampleInputPassword1">Password</label>
                             <input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="Password">
-                            <small>{{ $errors ? $errors->first('password') : '' }}</small>
+                            <small style="color: red">{{ $errors ? $errors->first('password') : '' }}</small>
                         </div>
                         <div class="form-group">
                             <label for="exampleInputPassword1">Password Confirmation</label>
